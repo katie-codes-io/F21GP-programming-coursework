254c18f6036ebe48ff80bb4cee95692c5877f3bc
Katie Baker <katie.m.baker87@gmail.com>
2021-02-18 14:25:01 +0000

    Updated KeyboardController. Added Player 2 controls.

--- Assets/Scripts/KeyboardController.cs
+++ Assets/Scripts/KeyboardController.cs
@@ -11,6 +11,13 @@ public class KeyboardController : MonoBehaviour {
     Animator animator;
     CharacterController controller;
 
+    public KeyCode keyUp    = KeyCode.W;
+    public KeyCode keyDown  = KeyCode.S;
+    public KeyCode keyLeft  = KeyCode.A;
+    public KeyCode keyRight = KeyCode.D;
+    public KeyCode keySprint = KeyCode.LeftShift;
+    public KeyCode keyAttack = KeyCode.Space;
+
     void Start() {
         animator = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
@@ -20,36 +27,36 @@ public class KeyboardController : MonoBehaviour {
 
         vector = Vector3.zero;
 
-        bool keyUp    = Input.GetKey(KeyCode.UpArrow);
-        bool keyDown  = Input.GetKey(KeyCode.DownArrow);
-        bool keyLeft  = Input.GetKey(KeyCode.RightArrow);     // the left and right arrows are reversed due to the fixed camera,
-        bool keyRight = Input.GetKey(KeyCode.LeftArrow);      // the object is facing the camera rather than the camera being behind
-        bool keyShift = Input.GetKeyDown(KeyCode.LeftShift);
-        bool keySpace = Input.GetKeyDown(KeyCode.Space);
+        bool pressingUp    = Input.GetKey(keyUp);
+        bool pressingDown  = Input.GetKey(keyDown);
+        bool pressingLeft  = Input.GetKey(keyRight);     // the left and right arrows are reversed due to the fixed camera,
+        bool pressingRight = Input.GetKey(keyLeft);      // the object is facing the camera rather than the camera being behind
+        bool pressingShift = Input.GetKeyDown(keySprint);
+        bool pressingSpace = Input.GetKeyDown(keyAttack);
 
         if (controller.isGrounded) {
 
             // Rotate
-            if (keyUp) {
+            if (pressingUp) {
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.15F);
             }
 
-            if (keyLeft) {
+            if (pressingLeft) {
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.15F);
             }
             
-            if (keyDown) {
+            if (pressingDown) {
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.15F);
             }
 
-            if (keyRight) {
+            if (pressingRight) {
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.15F);
             }
 
             // Moving
-            if (keyDown | keyLeft | keyRight | keyUp) {
+            if (pressingDown | pressingLeft | pressingRight | pressingUp) {
                 // Check if shift button is pressed for sprinting
-                if (keyShift) {
+                if (pressingShift) {
                     // Sprint animation
                     animator.SetFloat("Run Blend", 1.5f);
                 } else {
@@ -63,19 +70,19 @@ public class KeyboardController : MonoBehaviour {
             }
 
             // Stopped moving
-            if (!keyDown & !keyLeft & !keyRight & !keyUp) {
+            if (!pressingDown & !pressingLeft & !pressingRight & !pressingUp) {
                 // Idle animation
                 animator.SetFloat("Run Blend", 0.0f);
             }
         
             // Attacking
-            if (keySpace) {
+            if (pressingSpace) {
                 // Attack animation
                 animator.SetBool("Attack", true);
             }
 
             // Stopped attacking
-            if (!keySpace) {
+            if (!pressingSpace) {
                 // Attack animation
                 animator.SetBool("Attack", false);
             }
