536271f16e39a478da6396712188e6f11dfe4591
Katie Baker <kb2000@hw.ac.uk>
2021-02-18 16:40:52 +0000

    Added trigger to shed

---
+++ Assets/Scripts/EnterShed.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterShed : MonoBehaviour
+{
+
+    private GameObject shedFront;
+    private GameObject shedRoof;
+    private GameObject[] objects = new GameObject[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the shed front wall and roof objects
+        shedFront = GameObject.Find("Front");
+        shedRoof  = GameObject.Find("Roof");
+        objects[0] = shedFront;
+        objects[1] = shedRoof;
+    }
+
+    void OnTriggerEnter (Collider other) {
+        Debug.Log("Enter");
+    }
+
+    void OnTriggerStay (Collider other) {
+        Debug.Log("Stay");
+    }
+
+    void OnTriggerExit(Collider other) {
+
+        // First check if other is a player
+        if (other.tag == "Player") {
+
+            // Adjust the transparency of the front wall and roof
+            foreach (GameObject obj in objects) {
+                var material = obj.GetComponent<Renderer>().material;
+                var color = material.color;
+                
+                // Switch transparency
+                if (color.a == 1.0f) {
+                    color.a = 0.3f;
+                } else {
+                    color.a = 1.0f;
+                }
+
+                // Set color with new transparency
+                material.color = color;
+            }
+        }
+    }
+}
